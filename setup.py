a2d9e6256894c35932dd50bbe8572b0f2fc20e2a
difra84 <pdifranc@tcd.ie>
2018-03-15 21:30:04 +0100

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='tbd',
-  version='1.0.0',
+  version='1.1.0',
   description='script to generate points, adjacency table, and plots',
   url='https://gitlab.com/pdifranc/tbd.git',
 	author='pdifranc',
